e8f43ae856c19d07a2ec053a8aec1dfe1952b0c4
unknown <liber2693@gmail.com>
2018-08-03 01:00:35 -0400

    poniendo pdf de invoice criminal

--- view/invoice_pdf.php
+++ view/invoice_pdf.php
@@ -13,10 +13,11 @@ if ($array->num_rows==0) {
   echo "NO EXIST";
 }else{
   $datos = $array->fetch_array();
+  //echo "<pre>";print_r($datos);die;
 
   $html = '<style>
 
-      h2{
+      h3{
         text-align:center;
       }
       p{
@@ -30,6 +31,10 @@ if ($array->num_rows==0) {
       table th{color: #ffffff; background-color: #3692cd;}
       .table-striped > tbody > tr > td{border:2px solid #ffffff; background-color: #d1f1fc;}
 
+      td {
+        height: 30px;
+        }
+
       .di{
         text-align:center
       }
@@ -38,11 +43,6 @@ if ($array->num_rows==0) {
         margin: 1px 0;
         font-family: Arial, Helvetica, sans-serif;
       }
-
-      td {
-        width:20px;
-        height: 20px;
-        }
 
       #codigo{
         float: right;
@@ -54,10 +54,45 @@ if ($array->num_rows==0) {
       }
       </style>';
 
-  $html.='<h2>INVOICE
+      $html.='<br>
+      <h3>INVOICE</h3>
+      <table border="1" width="100%">
+        <thead>
+          <tr>
+            <td colspan="2px" width="20%"><center><b>DOCKET: &nbsp;'.$datos['codigo_docket'].'</b></center></td>
+            <td colspan="2px" width="20%"><b><center>INVOICE: &nbsp;'.$datos['codigo_invoice'].'</b></center></td>
+            <td colspan="2px" width="25%"><b><center>DATE: &nbsp;'.$datos['fecha_creacion'].'</b></center></td>
+            <td colspan="2px" width="35%"><b><center>BILL TO: &nbsp;'.ucwords($datos['cliente']).'</b></center></td>
+          </tr>
+        </thead>
+      </table>
+
+      <table border="1" width="100%">
+        <thead>
+          <tr>
+            <td colspan="2px" width="30%"><center><b>ORIGIN: &nbsp;'.$datos['pais_origen'].', '.ucfirst($datos['lugar_origen']).'</b></center></td>
+            <td colspan="2px" width="30%"><b><center>DESTINATION: &nbsp;'.$datos['pais_destino'].', '.ucfirst($datos['lugar_destino']).'</b></center></td>
+            <td colspan="2px" width="20%"><b><center>PIECES: &nbsp;'.$datos['pieza'].' '.ucfirst($datos['tipo_pieza']).'</b></center></td>
+            <td colspan="2px" width="20%"><b><center>WEIGHT: &nbsp;'.$datos['peso'].' '.ucfirst($datos['tipo_peso']).'</b></center></td>
+          </tr>
+        </thead>
+      </table>
+
+      <table border="1" width="100%">
+        <thead>
+          <tr>
+            <td colspan="1px" width="40%"><center><b>DIMENSIONS: &nbsp;'.$datos['alto'].' X '.$datos['ancho'].' X '.$datos['largo'].'  '.ucfirst($datos['tipo_dimension']).'</b></center></td>
+            <td colspan="7px" width="60%"><b><center>NOTE: &nbsp;'.ucfirst($datos['descripcion']).'</b></center></td>
+          </tr>
+        </thead>
+      </table>';
+        //print_r($html);die;
+//        width:60px;
+
+  /*$html.='<h2>INVOICE
             <p>
               DOCKET: &nbsp;'.$datos['codigo_docket'].'<br>
-              INVOICE: &nbsp;'.$datos['codigo_invoice'].'
+              INVOICE: &nbsp;'.$datos['codigo_invoice'].'<br>
               DATE: &nbsp;'.$datos['fecha_creacion'].'<br>
               ORIGIN: &nbsp;'.$datos['pais_origen'].', '.ucfirst($datos['lugar_origen']).'<br>
               DESTINATION: &nbsp;'.$datos['pais_destino'].', '.ucfirst($datos['lugar_destino']).'<br>
@@ -65,30 +100,32 @@ if ($array->num_rows==0) {
               PIECES: &nbsp;'.$datos['pieza'].' '.ucfirst($datos['tipo_pieza']).'<br>
               WEIGHT: &nbsp;'.$datos['peso'].' '.ucfirst($datos['tipo_peso']).'<br>
               DIMENSIONS: &nbsp;'.$datos['alto'].' X '.$datos['ancho'].' X '.$datos['largo'].'  '.ucfirst($datos['tipo_dimension']).'<br>
-              NOTE: &nbsp;'.ucfirst($datos['descripcion']).'<br>
+              NOTES: &nbsp;'.ucfirst($datos['descripcion']).'<br>
             </p>
           </h2>';
-
+*/
   $buscarServInvoice = invoicesServices::soloCodigo($codigo_factura);
   $array1 = $buscarServInvoice->SelectServicosInvoice();
 
+  //echo "<pre>";print_r($array1);die;
+
   $html.='
-  <h2>Services</h2>
+  <h3>Services</h3>
   <table border="1" width="100%">
     <thead>
       <tr>
-        <td colspan="2px"><center><b>#</b></center></td>
-        <td colspan="3px"><b><center>Description</b></center></td>
-        <td colspan="2px"><b><center>US$</b></center></td>
-        <td colspan="2px"><b><center>CAD$</b></center></td>
-        <td colspan="8px"><b><center>Notes</b></center></td>
+        <td width="10%"><center><b>#</b></center></td>
+        <td width="20%"><b><center>Description</b></center></td>
+        <td width="20%"><b><center>US$</b></center></td>
+        <td width="20%"><b><center>CAD$</b></center></td>
+        <td width="30%"><b><center>Notes</b></center></td>
       </tr>
     </thead>';
   if($array1->num_rows==0){
      $html.='
     <tbody>
       <tr>
-        <td colspan="5" class="text-center">No services</td>
+        <td colspan="5" width="100%"><center>No services</center></td>
       </tr>
     </tbody>';
   }else{
@@ -98,20 +135,15 @@ if ($array->num_rows==0) {
     $html.='
       <tbody>
         <tr>
-          <td colspan="2px">
-            <p>'.$i.'</p>
+          <td width="10%"><center>'.$i.'</center>
           </td>
-          <td colspan="3px">
-            <p>'.$datos_servi['descripcion'].'</p>
+          <td width="25%"><center>'.$datos_servi['descripcion'].'</center>
           </td>
-          <td colspan="2px">
-            <p>'.$datos_servi['precio_us'].'</p>
+          <td width="20%"><center>'.$datos_servi['precio_us'].'</center>
           </td>
-          <td colspan="2px">
-            <p>'.$datos_servi['precio_ca'].'</p>
+          <td width="20%"><center>'.$datos_servi['precio_ca'].'</center>
           </td>
-          <td colspan="8px">
-          <p>'.$datos_servi['nota'].'</p>
+          <td width="25%"><center>'.$datos_servi['nota'].'</center>
           </td>
         </tr>
       </tbody>';
@@ -122,20 +154,20 @@ if ($array->num_rows==0) {
   $buscarSupplierInvoice = SupplierInvoice::soloCodigo($codigo_factura);
   $array3 = $buscarSupplierInvoice->SelectProvedorInvoice();
   $html.='
-  <h2>Suplier</h2>
+  <h3>Supplier</h3>
   <table border="1" width="100%">
     <thead>
       <tr>
-        <td>#</td>
-        <td>Supplier</td>
-        <td>US$ AMT</td>
+        <td width="10%"><center><b>#</b></center></td>
+        <td width="30%"><center><b>Supplier</b></center></td>
+        <td width="20%"><center><b>COST US$</b></center></td>
       </tr>
     </thead>';
     if($array3->num_rows==0){
       $html.='
     <tbody>
       <tr>
-        <td colspan="5" class="text-center">NO SERVICES</td>
+        <td><center>NO SERVICES</center></td>
       </tr>
     </tbody>';
     }else{
@@ -145,15 +177,9 @@ if ($array->num_rows==0) {
       $html.='
       <tbody>
         <tr>
-          <td>
-            <p>'.$i.'</p>
-          </td>
-          <td>
-            <p>'.ucwords($datos_supli['supplier']).'</p>
-          </td>
-          <td>
-            <p>'.$datos_servi['dinero'].'</p>
-          </td>
+          <td><center>'.$i.'</center></td>
+          <td><center>'.ucwords($datos_supli['supplier']).'</center></td>
+          <td><center>'.$datos_servi['dinero'].'</center></td>
         </tr>
       </tbody>';
       }
@@ -165,12 +191,12 @@ if ($array->num_rows==0) {
   $array2 = $buscarViaEnvio->SelectViaEnvio();
 
   $html.='
-  <h2>Ship via</h2>';
+  <h3>Ship Via</h3>';
 
   if($array2->num_rows==0){
     $html.='
       <div>
-        No ship via
+        <center>No ship via</center>
       <div>';
   }else{
     $html.='
@@ -182,8 +208,8 @@ if ($array->num_rows==0) {
     $i++;
     $nota = ($datos2['id_envio']==6) ? ucfirst($datos2['nota']) : "" ;
       $html.='
-          <td>
-            <p>'.$datos2['descripcion'].'</p><b>'.$nota.'</b>
+          <td width="20%">
+            <b><center>'.$datos2['descripcion'] .$nota.'</b>
           </td>';
     }
   }
@@ -196,10 +222,6 @@ if ($array->num_rows==0) {
 }
 
 
-//==============================================================
-//==============================================================
-//==============================================================
-
 $mpdf=new mPDF('c','A4','','l',10,10,10,10,16,13);
 $mpdf->SetTitle('Invoice - '.$datos['codigo_invoice']);
 
@@ -210,6 +232,9 @@ $mpdf->SetTitle('Invoice - '.$datos['codigo_invoice']);
 // LOAD a stylesheet
 //$stylesheet = file_get_contents('mpdfstyletables.css');
 //$mpdf->WriteHTML($stylesheet,1);  // The parameter 1 tells that this is css/style only and no body/html/text
+$mpdf->SetHTMLHeader('<div><img src="../theme/img/logonetex.png" style="margin-left:1%;margin-top:-45px;width:160px;height:90px;"></div>');
+$mpdf->SetWatermarkImage("../theme/img/logonetex-bspline.png");
+$mpdf->showWatermarkImage = true;
 $mpdf->AddPage('L','','','','',10,10,10,10,16,13);
 $mpdf->WriteHTML($html);
 
@@ -224,9 +249,8 @@ exit;
       //$mpdf=new mPDF('c', 'LETTER','','',30,30,30,30);
       //
 
-      /*$mpdf->SetHTMLHeader('<div><img src="images/ultimo_cintillo.png" width="100%", height="55px"></div>');
-      $mpdf->SetWatermarkImage("images/fondo.png");
-      $mpdf->showWatermarkImage = true;
+      /*
+
       //$mpdf->SetWatermarkText('No válido');
       //$mpdf->showWatermarkText = true;
       //$mpdf->SetHTMLFooter(' <div  id="footer" ><img src="images/footer.png" width="100%"></div>');
